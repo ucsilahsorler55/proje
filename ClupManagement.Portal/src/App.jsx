03b7b73be337be3b19cedef7d1bf111e076c0c1b
Mehmet Ali <mehmetali.duran072002@gmail.com>
2025-10-23 01:52:23 +0300

    Refactor project structure and styles
    - Removed the old index.css file and replaced it with a new index.css in the styles directory.
    - Updated main.jsx to use ReactDOM instead of StrictMode for rendering the App component.
    - Added a new icon.svg file for the application icon.
    - Created a new Dashboard component with a user list table using Ant Design.
    - Introduced App.css for general application styling.

--- ClupManagement.Portal/src/App.jsx
+++ ClupManagement.Portal/src/App.jsx
@@ -1,17 +1,9 @@
-import { useState } from "react";
-import "./App.css";
+import "./styles/App.css";
 import "antd/dist/reset.css";
-import { Button } from "antd";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-    return (
-        <>
-            <div>VERİTABANINDAN GELEN YAZI BURADA OLACAK</div>
-            <h1>Ant Design Örneği</h1>
-            <Button type="primary">Kaydet</Button>
-            <Button danger>Sil</Button>
-        </>
-    );
+    return <Dashboard />;
 }
 
 export default App;
