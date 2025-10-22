a6b308867065c1fa7922c51160a0e736ebcb9925
Mehmet Ali <mehmetali.duran072002@gmail.com>
2025-10-23 01:09:50 +0300

    feat: add Ant Design library and integrate buttons in App component
    - Added Ant Design as a dependency in package.json
    - Imported Ant Design styles in App.jsx and main.jsx
    - Updated App component to include Ant Design buttons with primary and danger types

--- ClupManagement.Portal/src/App.jsx
+++ ClupManagement.Portal/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import "antd/dist/reset.css";
+import { Button } from "antd";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             <div>VERİTABANINDAN GELEN YAZI BURADA OLACAK</div>
+            <h1>Ant Design Örneği</h1>
+            <Button type="primary">Kaydet</Button>
+            <Button danger>Sil</Button>
         </>
     );
 }
