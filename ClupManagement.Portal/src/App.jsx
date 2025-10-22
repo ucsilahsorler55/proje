3de71c0b8fa8683117aeb19c03eb6439f791515e
Mehmet Ali <mehmetali.duran072002@gmail.com>
2025-10-23 00:25:37 +0300

    first commit

---
+++ ClupManagement.Portal/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <div>VERİTABANINDAN GELEN YAZI BURADA OLACAK</div>
+        </>
+    );
+}
+
+export default App;
